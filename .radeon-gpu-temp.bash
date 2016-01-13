98a1512c206be510b7fe99dd5c0acec4ba8d11fd
Alexandre Gouger <alexandregouger@gmail.com>
2016-01-12 22:31:26 -0500

    Update .radeon-gpu-temp.bash

--- .radeon-gpu-temp.bash
+++ .radeon-gpu-temp.bash
@@ -3,7 +3,7 @@
 i3status | (read line && echo $line && read line && echo $line && while :
 do
 	read line
-  cpu=$(sensors | grep temp1 | awk '{print $2;}' | tr -d '+')
+  	cpu=$(sensors | grep temp1 | awk '{print $2;}' | tr -d '+')
 	gpu=$(aticonfig --adapter=0 --od-gettemperature | grep Sensor | awk '{print $5;}' | cut -c -2)
 	text="[{ \"full_text\": \" Athlon x4 860K: ${cpu}\" },{ \"full_text\": \" Radeon R7 370: ${gpu}°C\" },"
 	echo  "${line/[/$text}" || exit 1
