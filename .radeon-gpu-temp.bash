c7d37b172942557f40b732695b0dd7d0660ec9af
Alexandre Gouger <alexandregouger@gmail.com>
2016-01-12 22:34:39 -0500

    Update .radeon-gpu-temp.bash

--- .radeon-gpu-temp.bash
+++ .radeon-gpu-temp.bash
@@ -1,10 +1,13 @@
 #!/bin/sh
-# shell script to prepend i3status with more stuff
+# Shell script to add CPU temperature and GPU temperature from an AMD GPU using proprietary driver
 i3status | (read line && echo $line && read line && echo $line && while :
 do
 	read line
+	
   	cpu=$(sensors | grep temp1 | awk '{print $2;}' | tr -d '+')
 	gpu=$(aticonfig --adapter=0 --od-gettemperature | grep Sensor | awk '{print $5;}' | cut -c -2)
+	
 	text="[{ \"full_text\": \" Athlon x4 860K: ${cpu}\" },{ \"full_text\": \" Radeon R7 370: ${gpu}°C\" },"
+	
 	echo  "${line/[/$text}" || exit 1
 done)
